f12af952728cf43ac1522bf522b7c97eb9046b31
Jackson Hagood <hagoojac@tamu.edu>
2022-03-25 16:40:40 -0500

    Add files via upload

--- Encryptor.py
+++ Encryptor.py
@@ -14,6 +14,7 @@ for char in m:
     c.append((key1 * ord(char) + key2) % 256)
 
 # print encrypted message as 8 bit binary numbers (starting and ending with a null character)
+
 binary += "11000001"
 for char in c:
     temp = bin(char)[2:]
@@ -24,11 +25,30 @@ print("Encrypted message: " + binary)
 
 dev = 2 # set using aplay -l
 
-for char in binary:
-    if char == '0':
-        SoundPlayer.playTone(500, 0.9, True, dev)
-        print("low")
-    else:
-        SoundPlayer.playTone(1000, 0.9, True, dev)
-        print("high")
-    time.sleep(0.1)
+p = SoundPlayer("z.wav", 0)
+p.play(0.5)
+time.sleep(1)
+
+# for i in range(0, len(binary), 2):
+#     if binary[i:i+2] == '00':
+#         print('A')
+#         SoundPlayer.playTone(200, 1, True, dev)
+#     elif binary[i:i+2] == '01':
+#         print('B')
+#         SoundPlayer.playTone(400, 1, True, dev)
+#     elif binary[i:i+2] == '10':
+#         print('C')
+#         SoundPlayer.playTone(800, 1, True, dev)
+#     else:
+#         print('D')
+#         SoundPlayer.playTone(1000, 1, True, dev)
+#     # time.sleep(1)
+
+# for char in binary:
+#     if char == '0':
+#         SoundPlayer.playTone(500, 0.5, False, dev)
+#         print("low")
+#     else:
+#         SoundPlayer.playTone(1000, 0.5, False, dev)
+#         print("high")
+#     time.sleep(0.6)
