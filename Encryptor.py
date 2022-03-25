c17a297acdf3cbfce58d884e42d21d6242dc06c3
Jackson Hagood <hagoojac@tamu.edu>
2022-03-25 09:26:16 -0500

    Add files via upload
    Skeleton code for outputting through a speaker

--- Encryptor.py
+++ Encryptor.py
@@ -4,14 +4,23 @@ key2 = 193
 
 m = input("Enter the message: ")
 c = []
+binary = ""
 
 for char in m:
     # encrypt each character with encryption formula
     c.append((key1 * ord(char) + key2) % 256)
 
 # print encrypted message as 8 bit binary numbers (starting and ending with a null character)
-print("Encrypted message: 11000001", end="")
+binary += "11000001"
 for char in c:
     temp = bin(char)[2:]
-    print('0' * (8 - len(temp)) + temp, end="")
-print("11000001")
+    binary += '0' * (8 - len(temp)) + temp
+binary += "11000001"
+
+print("Encrypted message: " + binary)
+
+# for char in binary:
+#     if char == '0':
+#         print("low")
+#     else:
+#         print("high")
