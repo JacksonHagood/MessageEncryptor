bd4a56f4b92b104bca4076e0988d6f90b625cfee
Jackson Hagood <hagoojac@tamu.edu>
2022-04-01 14:14:17 -0500

    Add files via upload

--- Encryptor.py
+++ Encryptor.py
@@ -1,5 +1,15 @@
-from soundplayer import SoundPlayer
-import time
+from pygame import mixer
+from time import sleep
+
+mixer.init()
+
+# mixer.music.load("D.mp3")
+# mixer.music.set_volume(0.5)
+# mixer.music.play()
+# sleep(1)
+# mixer.music.stop()
+
+# from playsound import playsound
 
 # affine cipher keys
 key1 = 7
@@ -23,32 +33,38 @@ binary += "11000001"
 
 print("Encrypted message: " + binary)
 
-dev = 2 # set using aplay -l
+for i in range(0, len(binary), 2):
+    if binary[i:i+2] == '00':
+        print('A')
+        mixer.music.load("A.mp3")
+        mixer.music.set_volume(0.5)
+        mixer.music.play()
+        sleep(1)
+        mixer.music.stop()
+        # SoundPlayer.playTone(200, 1, True, dev)
+    elif binary[i:i+2] == '01':
+        print('B')
+        mixer.music.load("B.mp3")
+        mixer.music.set_volume(0.5)
+        mixer.music.play()
+        sleep(1)
+        mixer.music.stop()
+        # SoundPlayer.playTone(400, 1, True, dev)
+    elif binary[i:i+2] == '10':
+        print('C')
+        mixer.music.load("C.mp3")
+        mixer.music.set_volume(0.5)
+        mixer.music.play()
+        sleep(1)
+        mixer.music.stop()
+        # SoundPlayer.playTone(800, 1, True, dev)
+    else:
+        print('D')
+        mixer.music.load("D.mp3")
+        mixer.music.set_volume(0.5)
+        mixer.music.play()
+        sleep(1)
+        mixer.music.stop()
+        # SoundPlayer.playTone(1000, 1, True, dev)
+    # time.sleep(1)
 
-p = SoundPlayer("z.wav", 0)
-p.play(0.5)
-time.sleep(1)
-
-# for i in range(0, len(binary), 2):
-#     if binary[i:i+2] == '00':
-#         print('A')
-#         SoundPlayer.playTone(200, 1, True, dev)
-#     elif binary[i:i+2] == '01':
-#         print('B')
-#         SoundPlayer.playTone(400, 1, True, dev)
-#     elif binary[i:i+2] == '10':
-#         print('C')
-#         SoundPlayer.playTone(800, 1, True, dev)
-#     else:
-#         print('D')
-#         SoundPlayer.playTone(1000, 1, True, dev)
-#     # time.sleep(1)
-
-# for char in binary:
-#     if char == '0':
-#         SoundPlayer.playTone(500, 0.5, False, dev)
-#         print("low")
-#     else:
-#         SoundPlayer.playTone(1000, 0.5, False, dev)
-#         print("high")
-#     time.sleep(0.6)
