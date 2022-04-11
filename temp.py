4b63b27890a43ed6ea1465dc7b6d430da1769fb4
Jackson Hagood <hagoojac@tamu.edu>
2022-04-11 12:17:49 -0500

    Add files via upload

--- temp.py
+++ temp.py
@@ -1,7 +1,53 @@
 import numpy as np
 from scipy.fft import *
 from scipy.io import wavfile
+import pyaudio
+import wave
+from scipy.io import wavfile
+import numpy as np
 import matplotlib.pyplot as plt
+import scipy.fft as fft
+from scipy.signal import find_peaks
+from textwrap import wrap
+
+# write to wave file
+CHUNK = 1000
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 44100
+RECORD_SECONDS = 12
+WAVE_OUTPUT_FILENAME = "input.wav"
+
+p = pyaudio.PyAudio()
+
+stream = p.open(format=FORMAT,
+                channels=CHANNELS,
+                rate=RATE,
+                input=True,
+                frames_per_buffer=CHUNK)
+
+print("* recording")
+
+frames = []
+
+for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+    data = stream.read(CHUNK)
+    frames.append(data)
+
+print("* done recording")
+
+stream.stop_stream()
+stream.close()
+p.terminate()
+
+wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+wf.setnchannels(CHANNELS)
+wf.setsampwidth(p.get_sample_size(FORMAT))
+wf.setframerate(RATE)
+wf.writeframes(b''.join(frames))
+wf.close()
+
+#------
 
 def freq(file, start_time, end_time):
     # Open the file and convert to mono
@@ -24,16 +70,18 @@ def freq(file, start_time, end_time):
     freq = xf[idx]
     return freq
 
-stop = 8000
-steps = 500
+# get length of wav file
+sample_rate, data = wavfile.read("input.wav")
+stop = int(len(data) / sample_rate) * 1000
+steps = 1000
 
 frequencies = []
 
 for i in range(steps):
-    # print(stop * ((i + 0.0) / 100.0), stop * ((i + 1.0) / 100.0))
+    if stop * ((i + 1.0) / steps) > stop:
+        break
+    
     frequencies.append(freq("input.wav", stop * ((i + 0.0) / steps), stop * ((i + 1.0) / steps)))
-
-    # print(freq("input.wav", stop * ((i + 0.0) / steps), stop * ((i + 1.0) / steps)))
 
 skip = 0
 
@@ -47,30 +95,48 @@ for f in frequencies:
         temp += "0"
         # print("0", end="")
 
-# print(temp, "\n")
+print(temp, "\n")
 
-# binary = ""
-# skip = 0
-# current = "0"
+binary = ""
+skip = 0
+current = "0"
 
-# for char in temp:
-#     if skip > 0:
-#         if char != current:
-#             skip = 0
-#         else:
-#             skip -= 1
-#     elif char == "0":
-#         binary += "0"
-#         current = "0"
-#         skip = 4
-#     else:
-#         binary += "1"
-#         current = "1"
-#         skip = 4
+for char in temp:
+    if current != char:
+        skip = 0
+    
+    if skip > 0:
+        skip -= 1
+    elif char == "0":
+        binary += "0"
+        current = "0"
+        skip = 8
+    else:
+        binary += "1"
+        current = "1"
+        skip = 8
 
-# print(binary, "\n")
+print(binary, "\n")
 
-# for i in range(len(binary)):
-#     if binary[i:i + 8] == "11000001":
+i = 0
+NULL_BYTE = "11000001"
+key1inv = 183
+key2 = 193
+m = ""
 
+for i in range(len(binary)):
+    if binary[i:i+8] == NULL_BYTE:
+        binary = binary[i + 8:]
+        msg_started = True
+        print("Start found")
+        break
 
+for j in range(0, len(binary), 8):
+    if binary[j:j+8] == NULL_BYTE:
+        binary = binary[:j]
+        print("End found")
+        break
+    else:
+        m += chr((key1inv * (int(binary[j:j+8], 2) - key2)) % 256)
+
+print("Message: " + m[0 : len(m) - 1])
